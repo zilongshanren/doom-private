974b65b1c2df96cce517d5c49874f345f7c645d9
zilongshanren <guanghui8827@126.com>
2021-12-01 21:39:28 +0800

    add misc funcs

--- +misc.el
+++ +misc.el
@@ -268,3 +268,34 @@ with options to run in the shell.")
 
 
 (setq counsel-git-cmd "git ls-files --full-name -- \":!:*.js.meta\" \":!:*.meta\"")
+
+(define-abbrev-table 'global-abbrev-table '(
+
+                                            ;; math/unicode symbols
+                                            ("8in" "∈")
+                                            ("8nin" "∉")
+                                            ("8inf" "∞")
+                                            ("8luv" "♥")
+                                            ("8smly" "☺")
+                                            ("8en" "@~english")
+                                            ("8zh" "@~chinese")
+                                            ("8sp" "spacemacs")
+                                            ;; email
+                                            ("8me" "guanghui8827@gmail.com")
+
+                                            ;; computing tech
+                                            ("8wp" "Wikipedia")
+                                            ("8ms" "Microsoft")
+                                            ("8g" "Google")
+                                            ("8it" "IntelliType")
+                                            ("8msw" "Microsoft Windows")
+                                            ("8win" "Windows")
+                                            ("8ie" "Internet Explorer")
+                                            ("8ahk" "AutoHotkey")
+                                            ("82dx" "Cocos2D-X")
+
+                                            ;; signature
+                                            ("8zl" "zilongshanren")
+                                            ;; emacs regex
+                                            ("8d" "\\([0-9]+?\\)")
+                                            ("8str" "\\([^\"]+?\\)\"")))
--- +org.el
+++ +org.el
@@ -11,3 +11,16 @@
 
 ;; If you intend to use org, it is recommended you change this!
 (setq org-directory "~/org-notes/")
+
+ ;; https://emacs-china.org/t/ox-hugo-auto-fill-mode-markdown/9547/4
+  (defadvice org-hugo-paragraph (before org-hugo-paragraph-advice
+                                        (paragraph contents info) activate)
+    "Join consecutive Chinese lines into a single long line without
+unwanted space when exporting org-mode to hugo markdown."
+    (let* ((origin-contents (ad-get-arg 1))
+           (fix-regexp "[[:multibyte:]]")
+           (fixed-contents
+            (replace-regexp-in-string
+             (concat
+              "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
+      (ad-set-arg 1 fixed-contents)))
